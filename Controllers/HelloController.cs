95f3760c8f5bb191a76879163e38ada35a5ec281
ThietNN <nguyenthiet1225@gmail.com>
2022-07-04 11:07:53 +0700

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using HelloWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.VBTest = "Thử viewbag xem thế nào";
+
+            var message = new MessageModel();
+            message.welcome = "Thử dùng model để tạo message ra màn hình xem sao";
+            return View(message);
+        }
+    }
+}
---
+++ Models/MessageModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloWorld.Models
+{
+    public class MessageModel
+    {
+        public string? welcome { get; set; }
+    }
+}
